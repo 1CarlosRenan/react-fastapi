c7fb58d7e9382584c67d9db6c84bf10b33dc329b
1CarlosRenan <carlosrenan96@gmail.com>
2022-09-25 23:12:42 -0300

    Criando os primeiros modelos com SQLAlchemy

---
+++ backend/database.py
@@ -0,0 +1,12 @@
+from sqlite3 import connect
+import sqlalchemy as _sql
+import sqlalchemy.ext.declarative as _declarative
+import sqlalchemy.orm as _orm
+
+DATABASE_URL = "sqlite:///./database.db"
+
+engine = _sql.create_engine(DATABASE_URL, connect_args={"check_same_thred": False})
+
+SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = _declarative.declarative_base()
